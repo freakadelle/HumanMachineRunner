dfc9d9c911e97b8fbf2943d949946c0c84d05629
Sarah Busert <sbusert@gmail.com>
2016-06-12 11:27:50 +0200

    UI on hoverboard

---
+++ Prototype_unityProject/Assets/WeightOverload.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WeightOverload : MonoBehaviour {
+
+    private Text _weightOverload;
+
+    // Use this for initialization
+    void Start () {
+        _weightOverload = GetComponent<Text>();
+        _weightOverload.enabled = false;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.C) && _weightOverload.enabled.Equals(false))
+        {
+            _weightOverload.enabled = true;
+	    }
+        else if (Input.GetKey(KeyCode.C) && _weightOverload.enabled.Equals(true))
+        {
+            _weightOverload.enabled = false;
+        }
+	}
+}
